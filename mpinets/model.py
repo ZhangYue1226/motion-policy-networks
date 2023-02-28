8cd5f4c42fcb3492eb463aeb8e59fdf469b6e4e1
ZhangYue1226 <93854600+ZhangYue1226@users.noreply.github.com>
2023-02-28 16:21:12 +0800

    Update model.py

--- mpinets/model.py
+++ mpinets/model.py
@@ -43,7 +43,7 @@ class MotionPolicyNetwork(pl.LightningModule):
         Constructs the model
         """
         super().__init__()
-        self.point_cloud_encoder = MPiNetsPointNet()
+        self.point_cloud_encoder = MPiNetsPointNet() # 点云编码器使用MPiNetsPointNet
         self.feature_encoder = nn.Sequential(
             nn.Linear(7, 32),
             nn.LeakyReLU(),
@@ -54,7 +54,7 @@ class MotionPolicyNetwork(pl.LightningModule):
             nn.Linear(128, 128),
             nn.LeakyReLU(),
             nn.Linear(128, 64),
-        )
+        ) # 特征编码器（即：robot configuration编码器）7-->64维
         self.decoder = nn.Sequential(
             nn.Linear(2048 + 64, 512),
             nn.LeakyReLU(),
@@ -63,18 +63,18 @@ class MotionPolicyNetwork(pl.LightningModule):
             nn.Linear(256, 128),
             nn.LeakyReLU(),
             nn.Linear(128, 7),
-        )
+        )  #解码器：2048--> 7维
 
     def configure_optimizers(self):
         """
-        A standard method in PyTorch lightning to set the optimizer
+        A standard method in PyTorch lightning to set the optimizer   PyTorch中设置优化器的标准方法
         """
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-4)
         return optimizer
 
     def forward(self, xyz: torch.Tensor, q: torch.Tensor) -> torch.Tensor:  # type: ignore[override]
         """
-        Passes data through the network to produce an output
+        Passes data through the network to produce an output 通过网络传递数据以产生输出
 
         :param xyz torch.Tensor: Tensor representing the point cloud. Should
                                       have dimensions of [B x N x 4] where B is the batch
@@ -85,10 +85,10 @@ class MotionPolicyNetwork(pl.LightningModule):
         :rtype torch.Tensor: The displacement to be applied to the current configuration to get
                      the position at the next step (still in normalized space)
         """
-        pc_encoding = self.point_cloud_encoder(xyz)
-        feature_encoding = self.feature_encoder(q)
-        x = torch.cat((pc_encoding, feature_encoding), dim=1)
-        return self.decoder(x)
+        pc_encoding = self.point_cloud_encoder(xyz) # xyz：点云的表达 格式：[B*N*4] 详见上面解释。本句：将xyz通过点云编码器处理后得到pc_encoding
+        feature_encoding = self.feature_encoder(q)  # q: 将robot configuration归一化至[-1,1]之后的张量。本句：将q通过特征编码器处理得到feature_encoding
+        x = torch.cat((pc_encoding, feature_encoding), dim=1) # 本句：将上述获得的两张量concat成为一个张量：x
+        return self.decoder(x) # 将x通过解码器，返回结果（qt点）
 
 
 class TrainingMotionPolicyNetwork(MotionPolicyNetwork):
@@ -98,6 +98,10 @@ class TrainingMotionPolicyNetwork(MotionPolicyNetwork):
     training process). This class is a valid model, but it's overkill when
     doing real robot inference and, for example, point cloud sampling is
     done by an outside process (such as downsampling point clouds from a point cloud).
+    
+    这是MotionPolicyNetwork模型的一个版本，它在训练期间(或在训练过程之外使用验证步骤)具有额外的必要属性。
+    这个类是一个有效的模型，但是当进行真正的机器人推理时，它是多余的，例如，点云采样是由外部过程完成的(例如从点云向下采样点云)。
+    在训练期间有用
     """
 
     def __init__(
@@ -108,6 +112,7 @@ class TrainingMotionPolicyNetwork(MotionPolicyNetwork):
     ):
         """
         Creates the network and assigns additional parameters for training
+        创建网络并为训练分配附加参数
 
 
         :param num_robot_points int: The number of robot points used when resampling
@@ -155,8 +160,9 @@ class TrainingMotionPolicyNetwork(MotionPolicyNetwork):
             batch["xyz"],
             batch["configuration"],
         )
-        # This block is to adapt for the case where we only want to roll out a
-        # single trajectory
+        # This block is to adapt for the case where we only want to roll out a single trajectory
+        # 这个块是为了适应我们只想展开单一轨迹的情况
+        
         if q.ndim == 1:
             xyz = xyz.unsqueeze(0)
             q = q.unsqueeze(0)
@@ -293,7 +299,7 @@ class TrainingMotionPolicyNetwork(MotionPolicyNetwork):
         B = batch["cuboid_centers"].size(0)
         rollout = torch.stack(rollout, dim=1)
         # Here is some Pytorch broadcasting voodoo to calculate whether each
-        # rollout has a collision or not (looking to calculate the collision rate)
+        # rollout has a collision or not (looking to calculate the collision rate) 这里有一些Pytorch广播巫术来计算每次rollout是否有碰撞(寻找计算碰撞率)
         assert rollout.shape == (B, 70, 7)
         rollout = rollout.reshape(-1, 7)
         has_collision = torch.zeros(B, dtype=torch.bool, device=self.device)
